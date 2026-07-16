b7dc1ae633bef39f2d24932d41782a8a6e79cdc8
sHacker927 <161908816+sHacker927@users.noreply.github.com>
2026-07-15 22:57:58 -0400

    Update clone.sh

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -34,6 +34,10 @@ REPO_DIR="Miserable_Xfce"
 # Remove existing directory
 [ -d "$REPO_DIR" ] && rm -rf "$REPO_DIR"
 
+RED='\e[31m'
+if [ -d /tmp/Miserable_Xfce]; then
+    echo "{$RED}Go to /tmp and delete the Miserable_Xfce directory!"
+
 # Clone and run installer
 cd /tmp
 git clone "$REPO_URL"
