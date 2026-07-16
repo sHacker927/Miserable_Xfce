72caa0d5b2df150c72de9873e165c0c2f6bb93f4
sHacker927 <161908816+sHacker927@users.noreply.github.com>
2026-07-15 23:00:26 -0400

    Update clone.sh

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -35,7 +35,7 @@ REPO_DIR="Miserable_Xfce"
 [ -d "$REPO_DIR" ] && rm -rf "$REPO_DIR"
 
 RED='\e[31m'
-if [ -d /tmp/Miserable_Xfce]; then
+if [[ -d /tmp/Miserable_Xfce]]; then
     echo "{$RED}Go to /tmp and delete the Miserable_Xfce directory!"
 fi
 
